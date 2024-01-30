1a8198bbea7b8d8c6c17abb5f7b9a18d4920cfea
JulienPoirierWebDev <connexion@julienpoirier-Webdev.com>
2024-01-30 17:56:49 +0100

    ju fix

--- plumes-projet-front/src/pages/home/index.jsx
+++ plumes-projet-front/src/pages/home/index.jsx
@@ -1,17 +1,14 @@
 import Typography from "../../components/common/Typography";
 
 const HomePage = () => {
+  return (
+    <div className={`underline bg-vert-naturaliste p-8`}>
+      <Typography tag="h2" variant="vert-naturaliste">
+        Page accueil
+      </Typography>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+    </div>
+  );
+};
 
-  
-    return (
-      <div className={`underline bg-vert-naturaliste p-8`}>
-        <Typography tag="h2" variant="vert-naturaliste">
-          Page accueil
-        </Typography>
-        <h1 className="underline">Bienvenue</h1>
-      </div>
-    );
-  }
-  
-  export default HomePage;
-  
+export default HomePage;
