776040b3dede1b1ca93d332891a96fb4117bb465
Cisco29 <castro.francisco@live.fr>
2024-02-01 09:09:13 +0100

    test de logo and co

--- plumes-projet-front/src/main.jsx
+++ plumes-projet-front/src/main.jsx
@@ -1,31 +1,39 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./layout/Layout.jsx";
-import HomePage from "./pages/home/index.jsx";
-import LexiquePage from "./pages/lexique/index.jsx";
-import { IdentifierPage, Header } from "./pages/identifier"; // Importez en tant que module nommé
-import EspecesPage from "./pages/especes/index.jsx";
-import "./index.css";
+import HomePage from  "./pages/home/index.jsx";
+import LexiquePage from  "./pages/lexique/index.jsx";
+import IdentifierPage from  "./pages/identifier";
+import EspecesPage from  "./pages/especes/index.jsx";
 
+
+import './index.css'
 
 const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      { path: "/", element: <HomePage /> },
-      { path: "/lexique", element: <LexiquePage /> },
-      { path: "/identifier", element: <IdentifierPage /> },
-      { path: "/especes", element: <EspecesPage /> },
-    ],
-  },
-]);
+    {
+      element: <Layout />,
+      children: [
+        { path: "/", element: <HomePage /> },
+        { path: "/lexique", element: <LexiquePage /> },
+        { path: "/identifier", element: <IdentifierPage/> },
+        { path: "/especes", element: <EspecesPage/> },
+      ],
+    },
+  ]);
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>
-  
-);
+
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router}></RouterProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+
+  // ReactDOM.createRoot(document.getElementById("root")).render(
+  //   <React.StrictMode>
+  //     <RouterProvider router={router}></RouterProvider>
+  //     </React.StrictMode> 
+  //   document.getElementById("root")
+
+  // );
