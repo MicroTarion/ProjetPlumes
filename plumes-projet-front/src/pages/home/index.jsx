f8d1374c78d45b759984b740098dc6c642b7f147
Cisco29 <castro.francisco@live.fr>
2024-02-03 05:49:16 +0100

    test bg linear gradient homePage

--- plumes-projet-front/src/pages/home/index.jsx
+++ plumes-projet-front/src/pages/home/index.jsx
@@ -1,21 +1,12 @@
-import LogoSvg from "../../../public/logo/Group 76.svg"; // Remplacez le chemin par le chemin réel de votre logo
+import LogoSvg from "../../../public/logo/Group 76.svg";
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-vert-naturaliste">
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-green-900 via-green-900 to-green-800 animate-gradient">
       <img src={LogoSvg} alt="Logo" className="mb-4" style={{ width: '14rem' }} />
 
       <div className={`p-8 text-center`}>
-        <h1 className="text-3xl font-bold" style={{
-          color: 'var(--Blanc-plume, #F8F8F8)',
-          textAlign: 'center',
-          fontFamily: 'Poppins',
-          fontSize: '16px',
-          fontStyle: 'normal',
-          fontWeight: 400,
-          lineHeight: '22px',
-          textDecoration: 'none', // Supprime le soulignement
-        }}>
+        <h1 className="text-3xl font-bold text-white font-poppins">
           Découvrez à quel oiseau appartient cette plume
         </h1>
       </div>
