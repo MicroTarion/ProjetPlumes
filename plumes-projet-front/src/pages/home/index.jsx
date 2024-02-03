eb5c022b0e59307b9a538ac26cb3d9028e2deb83
Cisco29 <castro.francisco@live.fr>
2024-02-03 12:02:14 +0100

    remise du background

--- plumes-projet-front/src/pages/home/index.jsx
+++ plumes-projet-front/src/pages/home/index.jsx
@@ -1,27 +1,26 @@
-
-
-import LogoSvg from "../../../public/logo/Group 76.svg"; 
-
-
-import Typography from "../../components/common/Typography";
-
-
+import LogoSvg from "../../../public/logo/Group 76.svg"; // Remplacez le chemin par le chemin réel de votre logo
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-green-900 via-green-900 to-green-800 animate-gradient">
+    <div className="flex flex-col items-center justify-center h-screen bg-vert-naturaliste">
       <img src={LogoSvg} alt="Logo" className="mb-4" style={{ width: '14rem' }} />
 
       <div className={`p-8 text-center`}>
-
-        <h1 className="text-3xl font-bold text-white font-poppins">
-
+        <h1 className="text-3xl font-bold" style={{
+          color: 'var(--Blanc-plume, #F8F8F8)',
+          textAlign: 'center',
+          fontFamily: 'Poppins',
+          fontSize: '16px',
+          fontStyle: 'normal',
+          fontWeight: 400,
+          lineHeight: '22px',
+          textDecoration: 'none', // Supprime le soulignement
+        }}>
           Découvrez à quel oiseau appartient cette plume
-        </Typography>
+        </h1>
       </div>
     </div>
   );
 };
-
 
 export default HomePage;
