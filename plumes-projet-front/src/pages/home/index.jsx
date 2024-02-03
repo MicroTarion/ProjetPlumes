97488f542df97f2fcc0960d039656503d4480e95
Cisco29 <castro.francisco@live.fr>
2024-02-03 12:51:17 +0100

    test background plume vidéo home

--- plumes-projet-front/src/pages/home/index.jsx
+++ plumes-projet-front/src/pages/home/index.jsx
@@ -1,23 +1,38 @@
-import LogoSvg from "../../../public/logo/Group 76.svg"; // Remplacez le chemin par le chemin réel de votre logo
+import LogoSvg from "../../../public/logo/Group 76.svg";
+import VideoBackground from "../../../public/videos/bgPlumes.mp4";
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-vert-naturaliste">
-      <img src={LogoSvg} alt="Logo" className="mb-4" style={{ width: '14rem' }} />
+    <div className="relative h-screen">
+      {/* Vidéo en arrière-plan */}
+      <video
+        className="absolute top-0 left-0 object-cover w-full h-full"
+        autoPlay
+        loop
+        muted
+        style={{ zIndex: -1 }} // Mettre le z-index à une valeur négative pour l'envoyer en arrière-plan
+      >
+        <source src={VideoBackground} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-      <div className={`p-8 text-center`}>
-        <h1 className="text-3xl font-bold" style={{
-          color: 'var(--Blanc-plume, #F8F8F8)',
-          textAlign: 'center',
-          fontFamily: 'Poppins',
-          fontSize: '16px',
-          fontStyle: 'normal',
-          fontWeight: 400,
-          lineHeight: '22px',
-          textDecoration: 'none', // Supprime le soulignement
-        }}>
-          Découvrez à quel oiseau appartient cette plume
-        </h1>
+      {/* Contenu au premier plan */}
+      <div className="flex flex-col items-center justify-center h-full bg-vert-naturaliste bg-opacity-50 relative" style={{ zIndex: 1 }}>
+        <img src={LogoSvg} alt="Logo" className="mb-4" style={{ width: '14rem' }} />
+        <div className={`p-8 text-center`}>
+          <h1 className="text-3xl font-bold" style={{
+            color: 'var(--Blanc-plume, #F8F8F8)',
+            textAlign: 'center',
+            fontFamily: 'Poppins',
+            fontSize: '16px',
+            fontStyle: 'normal',
+            fontWeight: 400,
+            lineHeight: '22px',
+            textDecoration: 'none', // Supprime le soulignement
+          }}>
+            Découvrez à quel oiseau appartient cette plume
+          </h1>
+        </div>
       </div>
     </div>
   );
