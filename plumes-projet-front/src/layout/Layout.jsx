98b143ef47200825cb8e53e3ddd8a7d8edcb946e
Julien POIRIER <poirierjulien@live.fr>
2024-02-06 12:31:39 +0100

    add responsive header

--- plumes-projet-front/src/layout/Layout.jsx
+++ plumes-projet-front/src/layout/Layout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/footer/index.jsx";
 import Header from "../components/header/index.jsx";
+import PageLayout from "./PageLayout.jsx";
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <PageLayout>
+        <Outlet />
+      </PageLayout>
       <Footer />
     </>
   );
